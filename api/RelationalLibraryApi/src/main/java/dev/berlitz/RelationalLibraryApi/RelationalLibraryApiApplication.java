cec07f04d99548b46dee5b8f003d25da930ec920
Marcelo Berlitz <x475682@gruposantander.com>
2021-11-25 13:47:52 -0300

    Initializes API

---
+++ api/RelationalLibraryApi/src/main/java/dev/berlitz/RelationalLibraryApi/RelationalLibraryApiApplication.java
@@ -0,0 +1,13 @@
+package dev.berlitz.RelationalLibraryApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelationalLibraryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelationalLibraryApiApplication.class, args);
+	}
+
+}
