314cc2119251664f603aa2adcfb6dd579983dc2e
Marcelo Berlitz <x475682@gruposantander.com>
2021-11-30 07:56:54 -0300

    add login request

--- client/src/api/users.ts
+++ client/src/api/users.ts
@@ -32,6 +32,11 @@ type UserResponse = {
   livros?: UserBookResponse[];
 };
 
+type LoginRequest = {
+  email: string;
+  senha: string;
+};
+
 export const createUser = (request: UserRequest): Promise<UserResponse> => {
   return ax.post("/", request).then((res) => res.data);
 };
@@ -53,4 +58,8 @@ export const getUser = (userCode: number): Promise<UserResponse> => {
 
 export const deleteUser = async (userCode: number): Promise<void> => {
   await ax.delete(`/${userCode}`).then((res) => res.data);
+};
+
+export const login = (request: LoginRequest): Promise<UserResponse> => {
+  return ax.post("/login", request).then((res) => res.data);
 };
