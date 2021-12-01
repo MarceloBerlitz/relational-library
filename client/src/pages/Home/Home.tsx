766293080685d9c04ba802000fc7c0d7aa21f9bd
Marcelo Berlitz <marceloberlitz@outlook.com>
2021-12-01 13:46:41 -0300

    implements book filter

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -6,26 +6,37 @@ import { useServices } from "../../providers/ServiceProvider";
 import { BookResponse } from "../../api/books";
 
 import "./styles.css";
+import { debounce } from "../../shared/debounce";
 
 const Home = () => {
   const { books } = useServices();
 
   const [booksState, setBooks] = useState<BookResponse[]>([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
-    books.getAllBooks().then(({ itens }) => setBooks(itens));
-  }, [books]);
+    books.getAllBooks(searchTerm).then(({ itens }) => setBooks(itens));
+  }, [searchTerm, books]);
+
   return (
     <>
       <Input
         className="home-component-search-book-input"
         placeholder="Livro de interesse"
+        onChange={(evt) => debounce(() => setSearchTerm(evt.target.value), 500)()}
       />
       <div className="home-component-card-container">
         {booksState.map(({ image, ...boook }) => (
           <Card
             className="home-component-card"
-            cover={<img className="home-component-card-image" src={image} alt="Cover"/>}
+            key={boook.codigo}
+            cover={
+              <img
+                className="home-component-card-image"
+                src={image}
+                alt="Cover"
+              />
+            }
           >
             {JSON.stringify(boook)}
           </Card>
