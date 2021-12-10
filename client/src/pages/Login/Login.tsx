afff6f5bc53ff726fb2b70ad053cb0aadc5b608d
Marcelo Berlitz <marceloberlitz@outlook.com>
2021-12-10 20:10:24 -0300

    Fix login page

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -16,41 +16,45 @@ const Login = () => {
     try {
       const result = await users.login(values);
       setUser(result);
-      navigate('/home')
+      navigate("/home");
     } catch {}
     setLoading(false);
   };
 
   return (
-    <Form
-      name="basic"
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 16 }}
-      initialValues={{ remember: true }}
-      onFinish={finishHandler}
-      autoComplete="off"
-    >
-      <Form.Item
-        label="E-mail"
-        name="email"
-        rules={[{ required: true, message: "Please input your email!" }]}
+    <div style={{ width: 500, margin: "150px auto" }}>
+      <h1 style={{ width: "100%" }}>Login</h1>
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        initialValues={{ remember: true }}
+        onFinish={finishHandler}
+        autoComplete="off"
+        layout="vertical"
       >
-        <Input />
-      </Form.Item>
+        <Form.Item
+          label="E-mail"
+          name="email"
+          rules={[{ required: true, message: "Please input your email!" }]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Form.Item
-        label="Senha"
-        name="senha"
-        rules={[{ required: true, message: "Please input your password!" }]}
-      >
-        <Input.Password />
-      </Form.Item>
-      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-        <Button type="primary" htmlType="submit" loading={loading}>
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item
+          label="Senha"
+          name="senha"
+          rules={[{ required: true, message: "Please input your password!" }]}
+        >
+          <Input.Password />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit" loading={loading}>
+            Entrar
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
   );
 };
 export default Login;
